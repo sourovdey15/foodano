9c92558852e8a1685237aea68b3499fddb7eb1d7
sourovdey15 <sourovdey2022@gmail.com>
2025-05-07 13:13:37 +0600

    Hot Deals Area

--- js/home.js
+++ js/home.js
@@ -4,10 +4,24 @@ const menuItems = document.querySelectorAll('li');
 menuItems.forEach(function(item){
     item.addEventListener('mouseover', () => {
         item.classList.add('hover:text-hoverColor');
-        item.classList.add('hover:underline');
+        // item.classList.add('hover:underline');
       });
       item.addEventListener('mouseout', () => {
         item.classList.remove('hover:text-hoverColor');
-        item.classList.remove('hover:underline');
+        // item.classList.remove('hover:underline');
       });
+});
+
+
+
+// button style
+document.querySelectorAll('button').forEach(button => {
+  // Wrap inner content in a <span> to handle layering
+  const span = document.createElement('span');
+  span.innerHTML = button.innerHTML;
+  button.innerHTML = '';
+  button.appendChild(span);
+
+  // Add the class for the hover effect
+  button.classList.add('circle-hover-effect');
 });
